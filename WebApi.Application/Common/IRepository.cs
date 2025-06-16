66b6da27f4e7912e28099d55a97cd79253f7d95b
Adhe Rama F <adhe.imcp@gmail.com>
2025-06-16 11:01:17 +0700

    complete base repository

--- WebApi.Application/Common/IRepository.cs
+++ WebApi.Application/Common/IRepository.cs
@@ -5,13 +5,26 @@ namespace WebApi.Application.Common
 {
     public interface IRepository<T> where T : BaseEntity
     {
-        Task<IList<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, List<string>? includeProperties = null, bool tracked = false);
+        Task<IList<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, bool tracked = false, params string[] includeProperties);
+        Task<(IList<T> Items, int TotalCount)> GetPagedAsync(
+            Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            int pageNumber = 1,
+            int pageSize = 10,
+            bool tracked = false,
+            params string[] includeProperties
+        );
+        Task<T?> GetOneAsync(Expression<Func<T, bool>> filter, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, bool tracked = false, params string[] includeProperties);
         Task<int> CountDataAsync(Expression<Func<T, bool>>? filter = null);
-        Task<T> GetByIdAsync(int id);
-        Task<T> GetByGuidAsync(Guid guid);
+        Task<T?> GetByIdAsync(int id, bool tracked = false);
+        Task<T?> GetByIdAsync(Guid id, bool tracked = false);
         Task CreateAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(T entity);
-        Task DeleteManyAsync(List<T> entity);
+        Task DeleteManyAsync(List<T> entities);
+        Task SaveChangesAsync();
+        Task BeginTransaction();
+        Task CommitTransaction();
+        Task RollbackTransaction();
     }
 }
---
+++ WebApi.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,170 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Linq.Expressions;
+using WebApi.Application.Common;
+using WebApi.Domain.Common;
+using WebApi.Infrastructure.Data;
+
+namespace WebApi.Infrastructure.Repository
+{
+    public class Repository<T>(AppDbContext dbContext) : IRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _dbContext = dbContext;
+        private readonly DbSet<T> _entities = dbContext.Set<T>();
+        private IDbContextTransaction? _transaction;
+
+        public async Task<IList<T>> GetAllAsync(
+            Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            bool tracked = false,
+            params string[] includeProperties)
+        {
+            IQueryable<T> query = _entities;
+
+            if (!tracked)
+                query = query.AsNoTracking();
+
+            if (filter is not null)
+                query = query.Where(filter);
+
+            if (orderBy is not null)
+                query = orderBy(query);
+
+            foreach (var includeProp in includeProperties)
+                query = query.Include(includeProp);
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T?> GetOneAsync(
+            Expression<Func<T, bool>> filter,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            bool tracked = false,
+            params string[] includeProperties)
+        {
+            IQueryable<T> query = _entities;
+
+            if (!tracked)
+                query = query.AsNoTracking();
+
+            query = query.Where(filter);
+
+            if (orderBy is not null)
+                query = orderBy(query);
+
+            foreach (var includeProp in includeProperties)
+                query = query.Include(includeProp);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<int> CountDataAsync(Expression<Func<T, bool>>? filter = null)
+        {
+            return filter is null
+                ? await _entities.CountAsync()
+                : await _entities.CountAsync(filter);
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id, bool tracked = false)
+        {
+            if (tracked)
+                return await _entities.FindAsync(id);
+
+            // Workaround for no-tracking with FindAsync
+            var entity = await _entities.FindAsync(id);
+            if (entity is not null)
+                _dbContext.Entry(entity).State = EntityState.Detached;
+
+            return entity;
+        }
+
+        public async Task<T?> GetByIdAsync(int id, bool tracked = false)
+        {
+            if (tracked)
+                return await _entities.FindAsync(id);
+
+            var entity = await _entities.FindAsync(id);
+            if (entity is not null)
+                _dbContext.Entry(entity).State = EntityState.Detached;
+
+            return entity;
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await _entities.AddAsync(entity);
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            _entities.Update(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            _entities.Remove(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteManyAsync(List<T> entities)
+        {
+            _entities.RemoveRange(entities);
+            return Task.CompletedTask;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task BeginTransaction()
+        {
+            _transaction = await _dbContext.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransaction()
+        {
+            if (_transaction is not null)
+            {
+                await _transaction.CommitAsync();
+                await _transaction.DisposeAsync();
+            }
+        }
+
+        public async Task RollbackTransaction()
+        {
+            if (_transaction is not null)
+            {
+                await _transaction.RollbackAsync();
+                await _transaction.DisposeAsync();
+            }
+        }
+
+        public async Task<(IList<T> Items, int TotalCount)> GetPagedAsync(Expression<Func<T, bool>>? filter = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, int pageNumber = 1, int pageSize = 10, bool tracked = false, params string[] includeProperties)
+        {
+            IQueryable<T> query = _entities;
+
+            if (!tracked)
+                query = query.AsNoTracking();
+
+            if (filter is not null)
+                query = query.Where(filter);
+
+            foreach (var includeProp in includeProperties)
+                query = query.Include(includeProp);
+
+            int totalCount = await query.CountAsync();
+
+            if (orderBy is not null)
+                query = orderBy(query);
+
+            var items = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return (items, totalCount);
+        }
+    }
+}
